678d99d956722c041cdb6bb71a4f3c31a8f2cc4c
acachathuranga <chhathuranga@gmail.com>
2020-06-29 18:47:41 +0800

    Named bag recorder

--- README.md
+++ README.md
@@ -2,16 +2,14 @@
 
 Bag Recorder package with ROS Service interface
 
-Some parts of the EKF algorithm were derived from https://github.com/utiasSTARS/pyshoe.git
-
 ## Dependencies
 * ROS
 * Python 
 
 #### Note:
->  Set the correct node name in the launch file. This name will be used to 'name' the recorder ROS service
+>  Set the correct paramters in config/parameters.yaml
 
 #### Services
-- std_srvs/Bool : /node_name_record_bag
+- std_srvs/Bool : /$(recorder_name)_bag
   
 
--- scripts/bag_recorder.py
+++ scripts/bag_recorder.py
@@ -6,6 +6,9 @@ import subprocess, shlex
 import os, signal
 
 rosbag_proc = None
+recorder_name = None
+topics = None
+directory = None
 
 def terminate_ros_node(s):
     list_cmd = subprocess.Popen("rosnode list", shell=True, stdout=subprocess.PIPE)
@@ -16,38 +19,38 @@ def terminate_ros_node(s):
         if (str.startswith(s)):
             os.system("rosnode kill " + str)
 
-
-def terminate_process_and_children(p):
-    ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % p.pid, shell=True, stdout=subprocess.PIPE)
-    ps_output = ps_command.stdout.read()
-    retcode = ps_command.wait()
-    assert retcode == 0, "ps command returned %d" % retcode
-    for pid_str in ps_output.split("\n")[:-1]:
-            os.kill(int(pid_str), signal.SIGINT)
-    p.terminate()
-
 def recordBag(req):
     global rosbag_proc
     if (req.data == True):
-        # # command = "rosbag record -O subset /camera/depth/image_raw /camera/rgb/image_raw /joy /mobile_base/sensors/imu_data_raw"
-        command = "rosbag record -a"
-        # command = shlex.split(command)
-        # rosbag_proc = subprocess.Popen(command)
-        rosbag_proc = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd="/home/sutd/Bag")
+        # command = "rosbag record -O subset /camera/depth/image_raw /camera/rgb/image_raw /joy /mobile_base/sensors/imu_data_raw"
+        # command = "rosbag record -a"
+        topicList = ""
+        for topic in topics:
+            topicList += "/" + topic + " "
+        
+        command = "roslaunch bag_recorder node.launch node_prefix:=" + recorder_name + " directory:=" + directory + " topics:='" + topicList + "'"
+        print (command)
+        rosbag_proc = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True)
         msg = "Started recording"
     else:
         if (rosbag_proc != None):
-            terminate_ros_node("/record")
-            # terminate_process_and_children(rosbag_proc)
-            # rosbag_proc.send_signal(subprocess.signal.SIGINT)
+            terminate_ros_node("/" + recorder_name + "_rosbag")
         msg = "Stopped recording"
     print (msg)
     return SetBoolResponse(True, msg)
 
 def bag_recorder():
-    rospy.init_node('bag_recorder')
-    s = rospy.Service('record_bag', SetBool, recordBag)
-    print ("Bag Recorder Ready!")
+    rospy.init_node('bag_recorder', anonymous=True)
+    node_name = rospy.get_name()
+
+    # Get Parameters
+    global recorder_name, topics, directory
+    recorder_name = rospy.get_param(rospy.get_name()+ '/recorder_name')
+    topics = rospy.get_param(rospy.get_name()+ '/topics', '-a')
+    directory = rospy.get_param(rospy.get_name()+ '/directory', '/home/sutd/Bag')
+
+    s = rospy.Service('/record_bag', SetBool, recordBag) #node_name + '/record_bag'
+    print recorder_name, " : Bag Recorder Ready!"
     rospy.spin()
 
 if __name__ == '__main__':
@@ -55,4 +58,5 @@ if __name__ == '__main__':
         bag_recorder()
     except rospy.ROSInterruptException:
         pass
+
 
